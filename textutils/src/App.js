a951e8bbc23a6c5461226f675cd0fea532ab1450
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-10-16 20:56:14 +0530

    initial commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,20 +1,64 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import DisplayComponent from './Components/DisplayComponent';
+import BtnComponent from './Components/BtnComponent';
 import './App.css';
 
-let name="Saba";
 function App() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
   return (
-     <>
-     <nav>
-       <li>Home</li>
-       <li>About</li>
-       <li>Contact</li>
-     </nav>
-     <h1>Hello {name}</h1>
-     <div className="container">
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nam, architecto odit eveniet suscipit illo et quia dignissimos perferendis libero ipsum qui autem rerum id! Molestiae quas non officia perferendis.</p>
+    <div className="main-section">
+     <div className="clock-holder">
+          <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
      </div>
-     </>
+    </div>
   );
 }
 
