1e749d69e810a37180e3ee2cb30b9be8eaff1948
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-10-19 17:05:47 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,50 @@
+import React,{Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      count:0
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     incrementCount = () => {
+     this.setState({
+        count:this.state.count+1
+     });
+  };
+
+ decrementCount=() => {
+    this.setState({
+       count:this.state.count-1
+    });
+ };
+
+ reset=() => {
+  this.state({
+     count:this.state.count-0
+  });
+};
+  
+ render() {
+     let {count}=this.state;
+     return (
+      <div className="App">
+         <div>
+           <div class="count">
+             <h2>Count:</h2>
+             <h1>{count}</h1>
+           </div>
+           <div class="button">
+             <Button title={"Increment"} action={this.incrementCount}/>
+             <Button title={"Decrement"} action={this.decrementCount}/>
+             <Button title="{Reset}" action={this.resetCount}/>
+           </div>
+         </div>
+      </div>
+    );
+}
 }
 
-export default App;
---
+++ my-app/src/components/Button.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+// import React{Component} from "react";
+
+export default class Button extends Component{
+    render(){
+        return <button onClick={this.props.action}>{this.props.title}</button>;
+    }
+}
